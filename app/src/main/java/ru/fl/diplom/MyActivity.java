53fe28d3afe9ba1fd74a83f7e1ffefb458a95401
Elena <me0wme0w@rambler.ru>
2015-04-14 14:00:52 +0400

    На третьей активности можно добавлять ингридиенты и их количество

--- app/src/main/java/ru/fl/diplom/MyActivity.java
+++ app/src/main/java/ru/fl/diplom/MyActivity.java
@@ -6,9 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/ru/fl/diplom/MyActivity3.java
+++ app/src/main/java/ru/fl/diplom/MyActivity3.java
@@ -1,15 +1,82 @@
 package ru.fl.diplom;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-public class MyActivity3 extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.ingridients);
-    }
+import java.util.ArrayList;
+import java.util.List;
+
+   public class MyActivity3 extends Activity {
+     private List<View> allEds;
+    // private int counter = 0;
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.ingridients);
+
+         Button addButton = (Button) findViewById(R.id.button);
+         allEds = new ArrayList<View>();
+         final LinearLayout linear = (LinearLayout) findViewById(R.id.linear);
+         addButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+
+         public void onClick(View v) {
+
+         //    counter++;
+             final View view = getLayoutInflater().inflate(R.layout.custom_edittext_layout, null);
+             Button deleteField = (Button) view.findViewById(R.id.button2);
+
+             deleteField.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     try {
+                         ((LinearLayout) view.getParent()).removeView(view);
+                         allEds.remove(view);
+                     }
+                     catch(IndexOutOfBoundsException ex) {
+                         ex.printStackTrace();
+                     }
+                 }
+             });
+
+             allEds.add(view);
+             linear.addView(view);
+
+             EditText text = (EditText) view.findViewById(R.id.editText);
+             text.setFilters(new InputFilter[]{
+                     new InputFilter() {
+                         @Override
+                         public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+
+                             if (source.toString().matches("[а-яА-ЯёЁ\\s]+")) {
+                                 return source;
+                             }
+                             return "";
+                         }
+                     }
+             });
+
+             EditText numb = (EditText) view.findViewById(R.id.editText4);
+             numb.setFilters(new InputFilter[]{
+                     new InputFilter() {
+                         @Override
+                         public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+
+                             if (source.toString().matches("[0-9а-яёА-ЯЁ\\s]+")) {
+                                 return source;
+                             }
+                             return "";
+                         }
+                     }
+             });
+         }
+         });
+     }
+
 }
