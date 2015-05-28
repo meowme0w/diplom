8a84af039aa0a06372ffdc53547ce6f3e41d5afb
Elena <me0wme0w@rambler.ru>
2015-05-28 18:24:12 +0400

    Создан слой и шаблон для заполнения

--- app/src/main/java/ru/fl/diplom/CategoriesActivity.java
+++ app/src/main/java/ru/fl/diplom/CategoriesActivity.java
@@ -1,7 +1,9 @@
 package ru.fl.diplom;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Елена on 09.05.2015.
@@ -12,5 +14,10 @@ public class CategoriesActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories_activity);
+    }
+
+    public void ShowCategoryRecipies (View view) {
+        Intent intent = new Intent(this,ResultActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ru/fl/diplom/ResultActivity.java
@@ -0,0 +1,16 @@
+package ru.fl.diplom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Елена on 28.05.2015.
+ */
+public class ResultActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_activity);
+    }
+}
